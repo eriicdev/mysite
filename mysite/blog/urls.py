3a108f8b8df54d16dd4cb48d66aa1babb8662635
Erik costa <ec5489976@gmail.com>
2025-09-02 22:20:56 -0300

    Criando os templates

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.PostView.as_view(), name='home'),
+    path("<slug:slug>/", views.PostDetail.as_view(), name='post_detail'),
 ]
